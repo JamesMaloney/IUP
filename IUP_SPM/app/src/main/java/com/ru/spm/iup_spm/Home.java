f7f10131b29229e2e239abff2276ec03b69e14f9
Smetz <matteo.guerrini@studenti.unicam.it>
2020-10-19 19:14:27 +0000

    Creating Home, Footer and Header done! :+1:

--- IUP_SPM/app/src/main/java/com/ru/spm/iup_spm/Home.java
+++ IUP_SPM/app/src/main/java/com/ru/spm/iup_spm/Home.java
@@ -1,17 +1,27 @@
 package com.ru.spm.iup_spm;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Home extends AppCompatActivity {
     LoginResponse loginResponse;
+    private BottomNavigationView buttonNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        buttonNavigationView = findViewById(R.id.bottom_nav);
+        buttonNavigationView.setOnNavigationItemSelectedListener(bottomNavMethod);
+        getSupportFragmentManager().beginTransaction().replace(R.id.container,new HomeFragment()).commit();
 
         Intent intent = getIntent();
         if (intent.getExtras()!= null){
@@ -20,5 +30,24 @@ public class Home extends AppCompatActivity {
         }
     }
 
-
+    private BottomNavigationView.OnNavigationItemSelectedListener bottomNavMethod =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            Fragment fragment=null;
+            switch (menuItem.getItemId()){
+                case R.id.home:
+                    fragment = new HomeFragment();
+                    break;
+                case R.id.events:
+                    fragment = new EventsFragment ();
+                    break;
+                case R.id.profile:
+                    fragment = new ProfileFragment();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.container,fragment).commit();
+            return true;
+        }
+    };
 }
--- IUP_SPM/app/src/main/java/com/ru/spm/iup_spm/MainActivity.java
+++ IUP_SPM/app/src/main/java/com/ru/spm/iup_spm/MainActivity.java
@@ -1,10 +1,11 @@
 package com.ru.spm.iup_spm;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     private Button buttonSignIn;
