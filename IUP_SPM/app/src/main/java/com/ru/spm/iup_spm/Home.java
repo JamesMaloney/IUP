70505dcd571064df40ba98c432d9f478ec8bad20
Smetz <matteo.guerrini@studenti.unicam.it>
2020-10-19 21:18:14 +0000

    Created Graphic aboud CreateEvents

--- IUP_SPM/app/src/main/java/com/ru/spm/iup_spm/Home.java
+++ IUP_SPM/app/src/main/java/com/ru/spm/iup_spm/Home.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,21 +16,38 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 public class Home extends AppCompatActivity {
     LoginResponse loginResponse;
     private BottomNavigationView buttonNavigationView;
+    private Button btnSettings;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        btnSettings = (Button) findViewById(R.id.btnSettings);
+        btnSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                open_settings();
+            }
+        });
 
         buttonNavigationView = findViewById(R.id.bottom_nav);
         buttonNavigationView.setOnNavigationItemSelectedListener(bottomNavMethod);
         getSupportFragmentManager().beginTransaction().replace(R.id.container,new HomeFragment()).commit();
+
 
         Intent intent = getIntent();
         if (intent.getExtras()!= null){
             loginResponse = (LoginResponse)intent.getSerializableExtra("data");
             Log.e("TAG","=========>>>> "+loginResponse.getToken());
         }
+
     }
+
+
+    private void open_settings() {
+        Intent intent_settings=new Intent(this,SettingsActivity.class);
+        startActivity(intent_settings);
+    }
+
 
     private BottomNavigationView.OnNavigationItemSelectedListener bottomNavMethod =
             new BottomNavigationView.OnNavigationItemSelectedListener() {
